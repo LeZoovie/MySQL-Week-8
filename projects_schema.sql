e333c51e3f2b8ba64a7fc566b3e30a234d3b5b28
Ricky <112603205+LeZoovie@users.noreply.github.com>
2022-11-03 15:41:03 -0600

    Project_schema
    Week 8 - MySQL - Drop table script

---
+++ projects_schema.sql
@@ -0,0 +1,50 @@
+
+DROP TABLE IF EXISTS project_category;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS step;
+DROP TABLE IF EXISTS material;
+DROP TABLE IF EXISTS project;
+
+CREATE TABLE project (
+	project_id INT AUTO_INCREMENT NOT NULL,
+	project_name VARCHAR (128) NOT NULL,
+	estimated_hours decimal (7, 2),
+    actual_hours decimal (7, 2),
+    difficulty INT,
+    notes TEXT,
+    primary key (project_id)
+);
+
+CREATE TABLE material (
+	material_id INT AUTO_INCREMENT NOT NULL,
+	project_id INT NOT NULL,
+	material_name VARCHAR (128) NOT NULL,
+    num_required INT,
+    cost decimal (7,2),
+    primary key (material_id),
+    FOREIGN KEY (project_id) references project (project_id)
+);
+
+CREATE TABLE step (
+	step_id INT AUTO_INCREMENT NOT NULL,
+	project_id INT NOT NULL,
+	step_text TEXT NOT NULL,
+	step_order INT NOT NULL,
+    primary key (step_id),
+	FOREIGN KEY (project_id) references project (project_id)
+);
+
+CREATE TABLE category (
+	category_id INT AUTO_INCREMENT NOT NULL,
+	category_name VARCHAR (64) NOT NULL,
+    primary key (category_id)
+);
+
+CREATE TABLE project_category(
+	project_id INT NOT NULL,
+	category_id INT NOT NULL,
+	FOREIGN KEY(project_id) references project (project_id),
+	FOREIGN KEY(category_id) references category (category_id),
+    UNIQUE KEY(project_id, category_id)
+
+);
